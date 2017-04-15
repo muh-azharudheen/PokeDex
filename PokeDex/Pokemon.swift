9cb7aa6cc6e925973c46fc51e1aa03a16b21bc6a
Mac <mac@Macs-MacBook-Pro.local>
2017-04-15 21:52:59 +0400

    Pokemon Class Created

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Mac on 4/15/17.
+//  Copyright © 2017 xerox101. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon{
+    
+    private var _name :String!
+    private var _pokedexId:Int!
+    
+    
+    
+    var name:String{
+        return _name
+    }
+    var pokedexId:Int{
+        return _pokedexId
+    }
+    
+    
+    init(name:String,pokedexId:Int){
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+}
